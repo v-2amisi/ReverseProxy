e385242d2182c579313b120241c04f3ddc308d3c
Amit <samitait2006@gmail.com>
2025-02-01 00:38:12 -0500

    session variable to bool for checking

--- Middlewares/ReverseProxyMiddleware.cs
+++ Middlewares/ReverseProxyMiddleware.cs
@@ -34,7 +34,7 @@ namespace CustomReverseProxy.Middlewares
 
             // Step 1: Determine if authentication is needed
             Console.WriteLine("reverseproxy middleware isauthenticated: " + context.User.Identity.IsAuthenticated.ToString());
-            bool authenticated = bool context.Session.GetString("IsAuthenticated");
+            bool authenticated = context.Session.GetString("IsAuthenticated");
             Console.WriteLine("context.Session.GetString('IsAuthenticated'): " + authenticated);
             string returnUrl = "https://ec2-54-82-60-31.compute-1.amazonaws.com:5001";
             if (IsProtectedRoute(requestedUrl) && !authenticated){
