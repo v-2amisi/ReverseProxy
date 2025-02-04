d8e390f1cb2c9379a1b1b7513a1788d04e9f2994
Amit <samitait2006@gmail.com>
2025-02-04 15:39:30 -0500

    fixed syntax errors

--- Middlewares/ReverseProxyMiddleware.cs
+++ Middlewares/ReverseProxyMiddleware.cs
@@ -51,7 +51,7 @@ namespace CustomReverseProxy.Middlewares
             else
             {
                 Console.WriteLine($"Calling build target message: {context.User.Identity.IsAuthenticated}");
-                Console.WriteLine("backendTarget: " + context.Request.Scheme + "://" + context.Request.Host + targetUri.AbsolutePath + targetUri.Query)
+                Console.WriteLine("backendTarget: " + context.Request.Scheme + "://" + context.Request.Host + targetUri.AbsolutePath + targetUri.Query);
                 var backendTarget = new Uri(context.Request.Scheme + "://" + context.Request.Host + targetUri.AbsolutePath + targetUri.Query);
                 var targetRequestMessage = CreateTargetMessage(context, backendTarget);
 
